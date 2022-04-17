63940ada4e896ae9e30642a8194961939ff870fe
Martin Stefanovic <martin.stefanovic@itcentar.rs>
2022-04-17 20:27:47 +0200

    Predavanje br. 4

---
+++ Predavanje Br. 4/js/uvod.js
@@ -0,0 +1,39 @@
+
+// ? Prvi nacin za kreiranje varijable
+// Definisanje varijable
+let broj
+// Dodeljivanje vrednosti varijabli
+broj = 23
+
+// ? Drugi nacin za kreiranje varijable
+const omiljeniBroj = 5
+let ime = 'Martin'
+
+// ? Stampanje vrednosti iz javascript-a
+// console.log('Pozdrav iz Javascripta', ime)
+// ? Stampanje varijable iz javascript-a
+//console.log(broj)
+// ? Kombinovanje vrednosti
+//console.log('Moj omiljeni broj je: ',omiljeniBroj)
+
+// ? Sabiranje: +
+// ? Oduzimanje: -
+// ? Mnozenje: *
+// ? Deteljnje: /
+let rezultat = ime + 5
+
+console.log('Rezultat je:',rezultat)
+
+console.log(omiljeniBroj)
+
+// omiljeniBroj = 15
+
+console.log('Omiljeni broj',omiljeniBroj)
+
+
+/*
+ Pisanje komentara
+ sa vise redova
+*/
+
+// Pisanje komentara u jednom redu
