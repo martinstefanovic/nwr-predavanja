d2c7e9d67cad701af91b63e12a1858cf630cd330
Martin Stefanovic <martin.stefanovic@itcentar.rs>
2022-04-26 13:49:54 +0200

    predavanje br 5

--- Predavanje Br. 4/js/uvod.js
+++ Predavanje Br. 4/js/uvod.js
@@ -1,13 +1,12 @@
-
 // ? Prvi nacin za kreiranje varijable
 // Definisanje varijable
-let broj
+let broj;
 // Dodeljivanje vrednosti varijabli
-broj = 23
+broj = 23;
 
 // ? Drugi nacin za kreiranje varijable
-const omiljeniBroj = 5
-let ime = 'Martin'
+const omiljeniBroj = 5;
+let ime = 'Martin';
 
 // ? Stampanje vrednosti iz javascript-a
 // console.log('Pozdrav iz Javascripta', ime)
@@ -20,16 +19,15 @@ let ime = 'Martin'
 // ? Oduzimanje: -
 // ? Mnozenje: *
 // ? Deteljnje: /
-let rezultat = ime + 5
+let rezultat = ime + 5;
 
-console.log('Rezultat je:',rezultat)
+console.log('Rezultat je:', rezultat);
 
-console.log(omiljeniBroj)
+console.log(omiljeniBroj);
 
 // omiljeniBroj = 15
 
-console.log('Omiljeni broj',omiljeniBroj)
-
+console.log('Omiljeni broj', omiljeniBroj);
 
 /*
  Pisanje komentara
@@ -37,3 +35,24 @@ console.log('Omiljeni broj',omiljeniBroj)
 */
 
 // Pisanje komentara u jednom redu
+
+let car = {
+  year: 2010,
+  brand: 'Audi',
+  model: 'a4',
+  isCarStarted: true,
+  start() {
+    console.log('Auto je upaljen');
+  },
+  stop() {
+    console.log('Auto je ugasen');
+  },
+  controls: {
+    left() {
+      console.log('Skrenuli smo levo');
+    },
+    right() {
+      console.log('Skrenuli smo desno');
+    },
+  },
+};
